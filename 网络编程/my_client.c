bb4d06a756713c835f927cad4d5b353f4fb96150
zzl13145 <1638331918@qq.com>
2022-10-25 15:21:52 +0800

    TCP,UDP

---
+++ 网络编程/my_client.c
@@ -0,0 +1,8 @@
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
