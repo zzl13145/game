7067d26cb6962aedb4324154616a35935f355f65
zzl13145 <1638331918@qq.com>
2022-10-25 14:16:46 +0800

    扫雷

---
+++ 扫雷（简易版）/game.h
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+#define EASY_COUNT 80
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols,char set);
+void DisplayBoard(char board[ROWS][COLS], int row, int col);
+void SetMine(char board[ROWS][COLS], int row, int col);
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
+//int get_mine_count(char mine[ROWS][COLS], int x, int y);
